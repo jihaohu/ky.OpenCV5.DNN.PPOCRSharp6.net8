da9a6cf4f3ab5e57f2592087a5003e34ac697dab
jihaohu <454396991@qq.com>
2026-06-13 17:12:05 +0800

    上传PPOCRSharp类库

---
+++ ky.OpenCV5.DNN.PPOCRSharp6/ImageSharpBgrConverter.cs
@@ -0,0 +1,24 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ky.OpenCV5.DNN.PPOCRSharp6
+{
+    public static class ImageSharpBgrConverter
+    {
+        public static byte[] ToBgrBytes(Image<Rgb24> image)
+        {
+            byte[] rgb = new byte[image.Width * image.Height * 3];
+            image.CopyPixelDataTo(rgb);
+            for (int i = 0; i < rgb.Length; i += 3)
+            {
+                (rgb[i], rgb[i + 2]) = (rgb[i + 2], rgb[i]);
+            }
+            return rgb;
+        }
+    }
+}
